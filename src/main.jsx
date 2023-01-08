109bc0a5239e376b64903c2d8acb20cf82f97142
agustinnarciande <narciande.2019@gmail.com>
2023-01-08 11:36:07 -0300

    Agregando admin

--- src/main.jsx
+++ src/main.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter ([
       {
         path :'/productos',
         element: <p className='font-bold text-6xl text-sky-700'> Aca se van a mostrar todos los productos</p>
+      },
+      {
+        path:'/admin',
+        element: <h2 className='font-bold text-4xl mt-24 text-center text-emerald-600'>Esta parte es solamente para administradores</h2>
       }
     ]
   }
